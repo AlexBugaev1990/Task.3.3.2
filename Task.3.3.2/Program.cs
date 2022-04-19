441c96b79b532ac2d7975ebe1a0e254a2cb3fb2d
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-04-19 20:50:05 +0300

    Добавьте файлы проекта.

---
+++ Task.3.3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task._3._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Alex";
+            byte MyAge = 31;
+            bool HaveIApet = false;
+            double MyShoeSize = 43;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+        }
+    }
+}
